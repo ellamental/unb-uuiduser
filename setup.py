793f10647c690c83a9f5683343e9680f9c42220d
Nick Zarczynski <nick@nickzarr.com>
2016-05-24 15:56:58 -0700

    UNB project layout standardization

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
-from setuptools import setup
+"""
+UUIDUser
+=========
+
+Django UUID-based User model.
+
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+VERSION_FILE_PATH = os.path.join(PROJECT_DIR, 'VERSION')
+
+
+def read_version():
+  if not os.path.isfile(VERSION_FILE_PATH):
+    raise EnvironmentError("Version file not found.")
+  with open(VERSION_FILE_PATH) as f:
+    return f.read().strip()
 
 
 if __name__ == '__main__':
   setup(
     name='unb-uuiduser',
-    version='0.0.5',
+    version=read_version(),
     description='Django UUID-based User model.',
-    author='Nick Zarczynski',
+    author='Nick Frezynski',
     author_email='nick@unb.services',
-    packages=[
-      'uuiduser',
-    ],
+    url='https://bitbucket.org/unbservices/unb-uuiduser',
+    license='MIT',
+    packages=['uuiduser'],
     include_package_data=True,
     install_requires=[],
-    license="MIT",
-    url='https://bitbucket.org/unbsolutions/unb-uuiduser',
     classifiers=[
       'Development Status :: 2 - Pre-Alpha',
       'Intended Audience :: Developers',
